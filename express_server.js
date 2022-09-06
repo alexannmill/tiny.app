eeb2b88eb35c3263a2d9947e1c13e1209651b1b2
alexannmill <alexannmill@gmail.com>
2022-09-06 00:41:39 +0000

    header added to view/partials, template added into index and show page

--- express_server.js
+++ express_server.js
@@ -2,18 +2,18 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/url.json", (req,res) => {
+app.get("/url.json", (req, res) => {
   res.json(urlDatabase);
 });
 
@@ -23,7 +23,10 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
   res.render("urls_show", templateVars);
 });
 
