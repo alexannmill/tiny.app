b0e4c426d77395486ed02c75234d0c9cd221c3a6
alexannmill <alexannmill@gmail.com>
2022-09-07 04:25:19 +0000

    added log in and out feater, cookies --- forgot to commit for the last few hours

--- express_server.js
+++ express_server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const morgan = require("morgan");
+const cookieParser = require('cookie-parser')
 
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.use(cookieParser())
 
 let urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
@@ -13,16 +15,41 @@ let urlDatabase = {
 };
 
 app.get("/url.json", (req, res) => {
+  const templateVars = { 
+    username: req.cookies["username"]
+   };
   res.json(urlDatabase);
 });
 
+app.post("/urls/login" , (req, res) => {
+const username = req.body.username
+// console.log(username)
+res.cookie("username", username)
+res.redirect("/urls");
+});
+
+app.post("/urls/logout" , (req, res) => {
+  const username = req.body.username
+  const templateVars = { 
+    username: req.cookies["username"]
+   };
+res.clearCookie("username", req.cookies["username"])
+res.redirect("/urls");
+});
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"]
+   };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -32,6 +59,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"],
   };
   res.render("urls_show", templateVars);
 });
@@ -40,8 +68,10 @@ app.post("/urls/:id", (req, res) => {
   const id = req.params.id
   const longURL = req.body.longURL
   urlDatabase[id] = longURL
-  console.log(urlDatabase)
-  res.redirect(`/urls/`)
+  const templateVars = { 
+    username: req.cookies["username"]
+   };
+  res.redirect(`/urls`)
   });
 
 app.post("/urls", (req, res) => {
@@ -61,7 +91,10 @@ app.post("/urls/:id/delete", (req, res) => {
 
 app.get("/u/:id", (req, res) => {
   longURL = urlDatabase[req.params.id];
-  res.redirect(longURL);
+  const templateVars = { 
+    username: req.cookies["username"]
+   };
+  res.redirect(longURL, templateVars);
 });
 
 app.listen(PORT, () => {
